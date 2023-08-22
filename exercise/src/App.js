6f9f66520c49efac3ffd3c12a67fbfcfd578d21f
baokhue <khue49945@donga.edu.vn>
2023-08-22 09:33:40 +0700

    hook

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Student from "./component/lesson3/Student";
 import {Counter1} from "./component/lesson4/Counter1";
 import {Counter2} from "./component/lesson4/hooks/Counter2";
+import TodoApp from "./component/lesson3/TodoApp";
 
 function App() {
   return (
     <>
         <Student />
+        <TodoApp />
         <Counter1 />
         <Counter2 />
+
     </>
   );
 }
--- exercise/src/component/lesson3/TodoApp.js
+++ exercise/src/component/lesson3/TodoApp.js
@@ -4,9 +4,22 @@ class TodoApp extends Component {
     constructor() {
         super();
         this.state = {
-            list: [],
-            item: ""
+            item: "",
+            list: ['Do homework']
         }
+    }
+
+    addItem() {
+        this.setState({
+            list: [...this.state.list, this.state.item],
+            item: ""
+        });
+    }
+
+    handleChange(value) {
+        this.setState({
+            item: value
+        })
     }
 
     render() {
@@ -14,7 +27,16 @@ class TodoApp extends Component {
             <>
                 <h1>Todo List</h1>
                 <input value={this.state.item} onChange={(event) => this.handleChange(event.target.value)} />
+                <button onClick={() => this.addItem()}>Add</button>
+
+                <ul>
+                    {this.state.list.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })}
+                </ul>
             </>
         )
     }
 }
+
+export default TodoApp;
